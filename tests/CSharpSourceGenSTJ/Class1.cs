7ac5d20e54f6917ef7e9d0772e010f69f058e075
BoundedChenn31 <BoundedChenn31@gmail.com>
2024-06-06 20:04:53 +0300

    Add stj benchmarks with source gen

---
+++ tests/CSharpSourceGenSTJ/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CSharpSourceGenSTJ;
+#nullable disable
+
+public enum Enum
+{
+    A = 0,
+    B = 1,
+    C = 3
+}
+
+public class Record
+{
+    public string FieldA { get; set; }
+    public int FieldB { get; set; }
+    public bool FieldC { get; set; }
+    public DateTimeOffset FieldD { get; set; }
+    public Nullable<Enum>[] FieldE { get; set; }
+    public string[] FieldZ { get; set; }
+}
+
+// type TestType = Record [][]
+
+// [JsonSourceGenerationOptions(WriteIndented = true)]
+[JsonSerializable(typeof(Record[][]))]
+internal partial class SourceGenContextDefault : JsonSerializerContext;
+
+[JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Serialization)]
+[JsonSerializable(typeof(Record[][]))]
+internal partial class SourceGenContextSerializationMode : JsonSerializerContext;
+
+[JsonSourceGenerationOptions(GenerationMode = JsonSourceGenerationMode.Metadata)]
+[JsonSerializable(typeof(Record[][]))]
+internal partial class SourceGenContextMetadataMode : JsonSerializerContext;
+
+public static class SourceGenSTJ
+{
+    public static Record[][] TestValue =
+    [
+        [
+            new Record
+            {
+                FieldA = "A", FieldB = 42, FieldC = true, FieldD = DateTimeOffset.MaxValue,
+                FieldE = [Enum.A, Enum.C, Enum.B], FieldZ = []
+            }
+        ],
+        [],
+        [
+            new Record
+            {
+                FieldA = "B", FieldB = 0, FieldC = false, FieldD = DateTimeOffset.MinValue, FieldE = [null, Enum.B], FieldZ = []
+            },
+            new Record
+            {
+                FieldA = "C", FieldB = -1, FieldC = false, FieldD = DateTimeOffset.MinValue, FieldE = [], FieldZ = ["A","B","C"]
+            }
+        ]
+    ];
+
+    public static string Serialize_Default(Record[][] testValue) =>
+        JsonSerializer.Serialize(testValue, SourceGenContextDefault.Default.RecordArrayArray);
+
+    public static Record[][] Deserialize_Default(string json) =>
+        JsonSerializer.Deserialize(json, SourceGenContextDefault.Default.RecordArrayArray);
+    
+    public static string Serialize_SerializationMode(Record[][] testValue) =>
+        JsonSerializer.Serialize(testValue, SourceGenContextSerializationMode.Default.RecordArrayArray);
+
+    public static Record[][] Deserialize_SerializationMode(string json) =>
+        JsonSerializer.Deserialize(json, SourceGenContextSerializationMode.Default.RecordArrayArray);
+    
+    public static string Serialize_MetadataMode(Record[][] testValue) =>
+        JsonSerializer.Serialize(testValue, SourceGenContextMetadataMode.Default.RecordArrayArray);
+
+    public static Record[][] Deserialize_MetadataMode(string json) =>
+        JsonSerializer.Deserialize(json, SourceGenContextMetadataMode.Default.RecordArrayArray);
+}
--- tests/TypeShape.Benchmarks/JsonSerializer.fs
+++ tests/TypeShape.Benchmarks/JsonSerializer.fs
@@ -2,6 +2,7 @@
 
 open System
 open BenchmarkDotNet.Attributes
+open CSharpSourceGenSTJ
 
 type Enum =
     | A = 0
@@ -23,25 +24,44 @@ type TestType = Record [][]
 
 [<MemoryDiagnoser>]
 type JsonSerializerBenchmarks() =
-    let converter = JsonSerializer.generateConverter<TestType>()
     // Value to be tested
-    let testValue : TestType = 
-        [|  
-            [| { FieldA = "A" ; FieldB = 42 ; FieldC = true ; FieldD = DateTimeOffset.MaxValue ; FieldE = [|Nullable Enum.A; Nullable Enum.C; Nullable Enum.B |] ; FieldZ = [||] } |]
-            [| |]
-            [| { FieldA = "B" ; FieldB = 0 ; FieldC = false ; FieldD = DateTimeOffset.MinValue ; FieldE = [|Nullable() ; Nullable Enum.B |] ; FieldZ = [||] } ;
-               { FieldA = "C" ; FieldB = -1 ; FieldC = false ; FieldD = DateTimeOffset.MinValue ; FieldE = [||] ; FieldZ = [|"A";"B";"C"|] } |]
-        |]
+    // let testValue : TestType = 
+    //     [|  
+    //         [| { FieldA = "A" ; FieldB = 42 ; FieldC = true ; FieldD = DateTimeOffset.MaxValue ; FieldE = [|Nullable Enum.A; Nullable Enum.C; Nullable Enum.B |] ; FieldZ = [||] } |]
+    //         [| |]
+    //         [| { FieldA = "B" ; FieldB = 0 ; FieldC = false ; FieldD = DateTimeOffset.MinValue ; FieldE = [|Nullable() ; Nullable Enum.B |] ; FieldZ = [||] } ;
+    //            { FieldA = "C" ; FieldB = -1 ; FieldC = false ; FieldD = DateTimeOffset.MinValue ; FieldE = [||] ; FieldZ = [|"A";"B";"C"|] } |]
+    //     |]
+    let converter = JsonSerializer.generateConverter<CSharpSourceGenSTJ.Record[][]>()
+    let testValue = SourceGenSTJ.TestValue
     let json = testValue |> System.Text.Json.JsonSerializer.Serialize
 
     [<Benchmark>]
-    member _.Serialize_SystemTextJson() = testValue |> System.Text.Json.JsonSerializer.Serialize
+    member _.Serialize_STJ_NoSGen() = testValue |> System.Text.Json.JsonSerializer.Serialize
+    
+    [<Benchmark>]
+    member _.Serialize_STJ_SGen_Default() = testValue |> SourceGenSTJ.Serialize_Default
+    
+    [<Benchmark>]
+    member _.Serialize_STJ_SGen_MetadataMode() = testValue |> SourceGenSTJ.Serialize_MetadataMode
+    
+    [<Benchmark>]
+    member _.Serialize_STJ_SGen_SerializationMode() = testValue |> SourceGenSTJ.Serialize_SerializationMode
         
     [<Benchmark>]
     member _.Serialize_TypeShape() = testValue |> JsonSerializer.serialize converter
 
     [<Benchmark>]
-    member _.Deserialize_SystemTextJson() = json |> System.Text.Json.JsonSerializer.Deserialize<TestType>
-        
+    member _.Deserialize_STJ_NoSGen() = json |> System.Text.Json.JsonSerializer.Deserialize<TestType>
+    
+    [<Benchmark>]
+    member _.Deserialize_STJ_SGen_Default() = json |> SourceGenSTJ.Deserialize_Default
+    
+    [<Benchmark>]
+    member _.Deserialize_STJ_SGen_MetadataMode() = json |> SourceGenSTJ.Deserialize_MetadataMode
+    
+    [<Benchmark>]
+    member _.Deserialize_STJ_SGen_SerializationMode() = json |> SourceGenSTJ.Deserialize_SerializationMode
+
     [<Benchmark>]
     member _.Deserialize_TypeShape() = json |> JsonSerializer.deserialize converter
